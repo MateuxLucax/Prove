fb9aa526909de9aee0951f1f2ca31879a9b2ffff
cristianpie <cristianpie2002@gmail.com>
2019-07-10 15:48:41 -0300

    restrição de acesso edição disciplinas

--- Integracao/Front-End/disciplina.php
+++ Integracao/Front-End/disciplina.php
@@ -1,5 +1,11 @@
 <!DOCTYPE html>
+
+<!-- ####################### ( SÓ VISUALIZA ) ##############################  -->
 <?php
+    //session_start();
+    // echo $_SESSION['matricula'];
+
+    include 'valida_secao.php';
 	include 'disciplinas_pdo.php';
 	include 'conf.php';
 	include 'funcoes.php';
@@ -31,40 +37,90 @@
 
 	<div id="professores">
 		<?php $reg_prof = selectPDO_discprof($codigo);
-			selectPDO_discprof_table($reg_prof); ?>
-		<button>Adicionar professor</button> <- deve abrir modal etc.
-		Embaixo: o que deve estar no modal:
-		<div id="adicao_professor" style="border:1px solid black; ">
-			<form action="disciplinas_pdo.php" method="post">
-				<?php gerarSelect($tb_professores, 'matricula', 0, 'Matricula', 'Nome'); ?>
-				<input type="hidden" name="disciplina" value="<?php echo $codigo; ?>">
-				<button type="submit" name="acao" id="acao" value="add_prof">Adicionar</button>
-			</form>
-		</div>
+			discprof_table ($reg_prof); ?>
 	</div>
 	<br/><br/><br/><br/><hr/><br/><br/><br/><br/>
 	<div id="alunos">
 		<?php $reg_alun = selectPDO_discalun($codigo);
-			selectPDO_discalun_table($reg_alun); ?> (Substituir essa tabela, gerada por uma função, por uma tabela na própria pagina)
-		<button>Adicionar alunos</button> <- deve abrir modal etc. <br/> Abaixo é o que deve estar no modal:</div>
+            
+            
+            discalun_table($reg_alun); ?> (Substituir essa tabela, gerada por uma função, por uma tabela na própria pagina)
+		
 		<div id="adicao_aluno" style="border:1px solid black; ">
 			<p>Segure o CTRL para selecionar vários alunos</p>
 			<form action="disciplinas_pdo.php" method="post">
 				<?php gerarSelectMultiple($tb_alunos, 'matriculas', 'Matricula', 'Nome'); ?>
 				<input type="hidden" name="disciplina" value="<?php echo $codigo ?>">
-				<button type="submit" name="acao" id="acao" value="add_alun">Adicionar</button>
 			</form>
 		</div>
 	</div>
 	<br/><br/><br/><br/><hr/><br/><br/><br/><br/>
 	<div id="avaliacoes">
 		<?php $reg_aval = selectPDO_discaval($codigo, 'disciplina');
-			selectPDO_discaval_table($reg_aval); ?>
-		<a href="cadastro_avaliacao.php">Adicionar avaliação</a>
+			discalun_table($reg_aval); ?>
 	</div>
+        
+
+
+
+		<br/><br/><br/>
+		<a href="disciplina_editar.php?codigo=<?php echo $codigo; ?>">Editar disciplina</a>
+
+        
+
+
 
 	<?php } else { ?>
 		<p><b>Erro:</b> A página não recebeu o código de uma disciplina</p>
 	<?php } ?>
+
+    <?php
+        function discalun_table ($registros) {
+            echo "<table class='highlight centered responsive-table' border='5'>
+            <thead class='black white-text'>
+            <tr>
+                <th>Matrícula</th>
+                <th>Nome</th>
+                <th>Código</th>
+                <th>Disciplina</th>
+            </tr>
+            </thead>
+            <tdbody>";
+        
+            for ($i=0; $i < count($registros); $i++) {
+                echo "<tr>";
+                for ($j=0; $j < count($registros[$i]); $j++) { 
+                    echo "<td>".$registros[$i][$j]."</td>";
+                }
+        
+                echo "<tr>";
+            }
+            echo "</tbody>
+            </table>";
+        }
+
+        function discprof_table ($registros) {
+            echo "<table class='highlight centered responsive-table' border='5'>
+            <thead class='black white-text'>
+            <tr>
+                <th>Matrícula</th>
+                <th>Nome</th>
+                <th>Código</th>
+                <th>Disciplina</th>
+            </tr>
+            </thead>
+            <tdbody>";
+        
+            for ($i=0; $i < count($registros); $i++) {
+                echo "<tr>";
+                for ($j=0; $j < count($registros[$i]); $j++) { 
+                    echo "<td>".$registros[$i][$j]."</td>";
+                }
+                echo "<tr>";
+            }
+            echo "</tbody>
+            </table>";
+        }
+    ?>
 </body>
 </html>
