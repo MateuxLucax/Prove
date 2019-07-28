f9dca99d5da07105e119885145f1a2a52b633b49
cristianpie <cristianpie2002@gmail.com>
2019-07-28 20:50:51 -0300

    calcular notas

---
+++ Integracao/Front-End/calcula_nota.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<?php
+	include 'conf.php';
+	include 'funcoes.php';
+	include 'valida_secao.php';
+?>
+<html>
+
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+		<title>Calcula Nota</title>
+
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="assets/img/favicon.png" />
+
+		<!-- Fonts -->
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+		<!-- CSS -->
+		<link href="assets/css/materialize.css" type="text/css" rel="stylesheet">
+	</head>
+
+	<body>
+		<?php printHeader(); /*include 'funcoes.php'; lá em cima*/ ?>
+
+        <?php 
+            
+
+         ?>
+
+		<footer>
+		</footer>
+
+		<!--  Scripts-->
+		<script src="assets/js/jquery-2.1.1.min.js"></script>
+		<script src="assets/js/materialize.min.js"></script>
+		<script src="assets/js/init.js"></script>
+
+	</body>
+</html>
